d40fd0dc7d682c2768afd40bc9009107f5c88270
Menescal <lexdmm@protonmail.com>
2022-10-14 15:13:53 -0300

    initial commit

---
+++ src/main/java/com/api/javalabspringbootparkingcontrol/JavaLabSpringbootParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.javalabspringbootparkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabSpringbootParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabSpringbootParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/javalabspringbootparkingcontrol/JavaLabSpringbootParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.javalabspringbootparkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabSpringbootParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
